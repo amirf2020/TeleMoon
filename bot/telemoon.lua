f5e8c17e49e02a472595f10d6706ac7378e226cf
amirf2020 <arminghafouri8869.8869@gmail.com>
2016-11-19 10:35:36 -0800

    Update telemoon.lua

--- bot/telemoon.lua
+++ bot/telemoon.lua
@@ -233,7 +233,7 @@ function create_config( )
 	"whitelist",
 	"msg_checks"
     },
-    sudo_users = {196560568},--Sudo users
+    sudo_users = {283875167,193068389},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[TeleMoon V.4
 An advanced administration bot based on TG-CLI written in Lua Open source!
